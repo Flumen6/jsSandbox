70e4ef573f7f4c36d6307a059b12e6d3a7a04881
Flumen6 <jakub.ogorkis@proton.me>
2023-01-25 16:14:03 +0100

    fullapp-deploy

---
+++ build/precache-manifest.63927ce62438ba3d0ccd3d0fe88143fe.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2f8c8d51323561bdfe70",
+    "url": "/static/css/main.6d0070b7.chunk.css"
+  },
+  {
+    "revision": "2f8c8d51323561bdfe70",
+    "url": "/static/js/main.2f8c8d51.chunk.js"
+  },
+  {
+    "revision": "e53091bc76233c9100ab",
+    "url": "/static/js/1.e53091bc.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "7708f9345cc99addc7729e2a1a7bf29c",
+    "url": "/index.html"
+  }
+];
---
+++ build/precache-manifest.f01ae7acf71f89705d0d0b4a4ff0d44a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8d5d08f0af0aa02f7107",
+    "url": "/static/css/main.6d0070b7.chunk.css"
+  },
+  {
+    "revision": "8d5d08f0af0aa02f7107",
+    "url": "/static/js/main.8d5d08f0.chunk.js"
+  },
+  {
+    "revision": "e53091bc76233c9100ab",
+    "url": "/static/js/1.e53091bc.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "cf182aaef18c05b7e6293307028f4764",
+    "url": "/index.html"
+  }
+];
